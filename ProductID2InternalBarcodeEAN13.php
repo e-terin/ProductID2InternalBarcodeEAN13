fce70355bd510433dd4380d530db459fb2318c85
Evgeniy Terin <62910242+evgeniy-terin@users.noreply.github.com>
2020-06-17 22:32:07 +0300

    Update ProductID2InternalBarcodeEAN13.php

--- ProductID2InternalBarcodeEAN13.php
+++ ProductID2InternalBarcodeEAN13.php
@@ -1,18 +1,23 @@
 <?php
-$id=(int) $_GET['id'];
-if($id) {
-    $ean13=($id+200000000000);
-    $ean13_str=(string)$ean13;
-    for($i=11;$i>=0;$i--) {
-        if(($i%2)==1) $m_nechet[]=(int) $ean13_str[$i];
-        else $m_chet[]=(int) $ean13_str[$i];
-    }
-    $sum=array_sum($m_chet)+3*array_sum($m_nechet);
-    $sum_str=(string)$sum;
-    $last_digit=(int)substr($sum_str, -1);
-    $control_summ=($last_digit==0)?0:(10-$last_digit);
-    $barcode=$ean13_str.$control_summ;
-    var_dump($barcode);
+
+namespace EvgeniyTerin\ProductID2InternalBarcodeEAN13;
+
+function getProductID2InternalBarcodeEAN13((int)$id) {
+	if($id) {
+		$ean13=($id+200000000000);
+		$ean13_str=(string)$ean13;
+		for($i=11;$i>=0;$i--) {
+			if(($i%2)==1) $m_nechet[]=(int) $ean13_str[$i];
+			else $m_chet[]=(int) $ean13_str[$i];
+		}
+		$sum=array_sum($m_chet)+3*array_sum($m_nechet);
+		$sum_str=(string)$sum;
+		$last_digit=(int)substr($sum_str, -1);
+		$control_summ=($last_digit==0)?0:(10-$last_digit);
+		$barcode=$ean13_str.$control_summ;
+		return $barcode;
+	}
+	else return false;
 
 }
 
